3e791537fad7dd8848ed1ad62626a441c97d630a
annoinspace <aneesahalmask@gmail.com>
2023-01-23 18:02:28 +0000

    image upload working

--- src/api/experiences/experiencesModel.js
+++ src/api/experiences/experiencesModel.js
@@ -2,16 +2,16 @@
 import mongoose from "mongoose"
 
 const { Schema } = mongoose
-const experiencesSchema = new Schema(
-  {
-    // user: [{ type: Schema.Types.ObjectId, ref: "Users" }],
-    role: { type: String, required: true },
-    company: { type: String, required: true },
-    startDate: { type: Date, required: true },
-    endDate: { type: Date },
-    description: { type: String, required: true },
-    image: { type: String }
-  },
-  { timestamps: true }
-)
+const experiencesSchema = new Schema({
+  // user: [{ type: Schema.Types.ObjectId, ref: "Users" }],
+  role: { type: String, required: true },
+  company: { type: String, required: true },
+  startDate: { type: Date, required: true },
+  endDate: { type: Date },
+  description: { type: String, required: true },
+  area: { type: String, required: true },
+  image: { type: String },
+  createdAt: { type: Date },
+  updatedAt: { type: Date }
+})
 export default experiencesSchema
